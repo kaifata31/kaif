1ab1e21e69a926f3c4975c8943028a4c63a635c3
kaifata31 <87989976+kaifata31@users.noreply.github.com>
2023-09-08 21:36:09 +0530

    Add files via upload

---
+++ insertNode.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+class Node{
+ public:
+int data;
+Node *next;
+
+Node(int data){
+    this->data=data;
+    next=NULL;
+}
+};
+Node* takeInput(){
+     int data;
+     
+     cin>>data;
+     Node *head=NULL;
+     Node *tail=NULL;
+     
+     while(data!=-1){
+      Node *newNode=new Node(data);
+      if(head==NULL){
+        head=newNode;
+        tail=newNode;
+      }else{
+        tail->next=newNode;
+        tail=tail->next;
+        // Node *temp=head;
+        // while(temp->next!=NULL){
+        //     temp=temp->next;
+        // }
+        // temp->next=newNode;
+      }
+      cin>>data;
+     }
+
+     return head;
+
+}
+Node* insertNode(Node *head,int i,int data){
+Node *newNode= new Node(data);
+
+int count=0;
+Node*temp=head; 
+if(i==0){
+    newNode->next=head;
+    head=newNode;
+    return  head;
+}
+ //while we travelling over a linked list use a temporary variable
+while( temp!=NULL && count<i-1){  //if we are using position greter than given
+temp=temp->next;
+count++;
+}
+if(temp!=NULL){  
+newNode->next=temp->next;/*This will be executed only when temp not equal to NULL*/
+temp->next=newNode;
+}
+  return head;
+}
+void print(Node *head){
+    //  Node *temp=head;
+    while(head!=NULL){
+        cout<<head->data<<endl;
+        head=head->next;
+    }
+}
+int main(){
+   Node*head= takeInput();
+    print(head);
+    int i,data;
+    cin>>i>>data;
+   head = insertNode(head,i,data);
+    print(head);
+
+}
